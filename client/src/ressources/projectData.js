c7f6ff37b6bf39ebbacbcf2ccfe4347dd95269a9
LOUTAG <tagliasco.lou@orange.fr>
2022-07-03 14:57:17 +0200

    add penblog project 02

--- client/src/ressources/projectData.js
+++ client/src/ressources/projectData.js
@@ -1,5 +1,14 @@
 const projectData = [
   {
+    title: "Penblog",
+    image: "./img/penblog.svg",
+    description:{
+      fr: "Réseau social inspiré de facebook et twitter (redux, auth: jwt, stockage images: cloudinary, style: Tailwind Css)",
+      en: "Social network inspired by facebook and twitter (redux, auth: jwt, image storage: cloudinary, styling: Tailwind Css)"
+    },
+    link: "https://arcane-peak-61091.herokuapp.com/"
+  },
+  {
     title: "DailyNews",
     image: "./img/dailynews.svg",
     description:{
@@ -7,15 +16,6 @@ const projectData = [
       en: "News portal app using MERN Stack with USER auth, all CRUD operations, popular text editor (draft), Tailwind Css for styling"
     },
     link: "https://dry-oasis-32135.herokuapp.com/"
-  },
-  {
-    title: "Penblog",
-    image: "./img/penblog.svg",
-    description:{
-      fr: "Réseau social inspiré de facebook et twitter",
-      en: "Social network inspired by facebook and twitter"
-    },
-    link: "https://arcane-peak-61091.herokuapp.com/"
   }
 ];
 export default projectData
